e795a13c42b21af8ab74bdce3884892a77065887
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-02-08 13:09:33 +0800

    添加布局通用组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import Loader from './Loader';
 import RoleUser from './RoleUser';
 import RoleStation from './RoleStation';
+export AssignLayout from './Layout/AssignLayout';
+export TreeView from './TreeView';
 
 export {
   Loader,
