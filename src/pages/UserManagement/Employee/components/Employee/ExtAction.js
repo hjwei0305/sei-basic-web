b41af62df6a2d623efdec73e2f63f90c3a3f4685
eason <cheng.yi@capitalcrux.com>
2020-08-05 15:26:48 +0800

    UE

--- src/pages/UserManagement/Employee/components/Employee/ExtAction.js
+++ src/pages/UserManagement/Employee/components/Employee/ExtAction.js
@@ -94,7 +94,7 @@ class ExtAction extends PureComponent {
       <>
         {menusData.length > 0 ? (
           <Dropdown
-            trigger={['hover', 'click']}
+            trigger={['click']}
             overlay={this.getMenu(menusData)}
             className="action-drop-down"
             placement="bottomLeft"
