6c58ef1b9e7a5692bafe2fcdcd28dde132fae4b0
张盼 <pan1.zhang@changhong.com>
2021-11-03 14:39:38 +0800

    更新src/pages/UserProfile/components/PaymentInfo/index.jsx

--- src/pages/UserProfile/components/PaymentInfo/index.jsx
+++ src/pages/UserProfile/components/PaymentInfo/index.jsx
@@ -190,6 +190,7 @@ class PaymentInfo extends Component {
         params: {
           receiverCode: basicInfo ? basicInfo.employeeCode : '',
           receiverType: 'H',
+          // test
           useScope: 'CASH',
         },
         url: `${SERVER_PATH}/dms/paymentInfo/findByReceiverCode`,
